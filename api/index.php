22b3381034a0cc8c01aee109064f476c46f0b148
Serroria <merylien.jovanda14@gmail.com>
2026-07-09 20:43:58 +0700

    fix for vercel hosting

--- api/index.php
+++ api/index.php
@@ -1,3 +1,13 @@
 <?php
-// Mengarahkan Vercel ke index Laravel yang asli
+
+
+if (!file_exists('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0777, true);
+}
+
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+
 require __DIR__ . '/../public/index.php';
