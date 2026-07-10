560caa7c2930a96b3356e589e63fd638fe25c9ba
Serroria <merylien.jovanda14@gmail.com>
2026-07-10 15:00:08 +0700

    vercel.json edited

--- api/index.php
+++ api/index.php
@@ -1,13 +1,21 @@
 <?php
 
+// Buat direktori sementara untuk Vercel (karena Vercel read-only)
+$directories = [
+    '/tmp/views',
+    '/tmp/cache',
+    '/tmp/sessions'
+];
 
-if (!file_exists('/tmp/storage/framework/views')) {
-    mkdir('/tmp/storage/framework/views', 0777, true);
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
 }
 
+// Redirect path Laravel ke folder /tmp
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+putenv('VIEW_COMPILED_PATH=/tmp/views');
 
-putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
-
-
+// Pastikan file index.php bawaan public Laravel dipanggil
 require __DIR__ . '/../public/index.php';
